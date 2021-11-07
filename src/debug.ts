0180f248a463f58be070411f8dfb3ea42f8b9dde
memanot <menemty@gmail.com>
2021-11-07 16:06:00 +0200

    .

--- src/debug.ts
+++ src/debug.ts
@@ -1,8 +1,23 @@
 /* tslint:disable:no-console */
 
 import { WtnSvc } from '.'
+import fetch from 'node-fetch'
 
 const debug = async () => {
+  const text = `The Eagles were focused primarily on moving players with expiring contracts, sources said, but Cox was brought up in some conversations, with the Steelers showing interest.`
+  const x = await fetch('https://api.wordtune.com/rewrite-limited', {
+    headers: {
+      'cache-control': 'no-cache',
+      'content-type': 'application/json'
+      //"userid": "deviceId-mQEG34Al9yPCMsSUnVK9s3",
+      //"x-wordtune-origin": "https://www.wordtune.com"
+    },
+    body: `{"action":"REWRITE","text":"${text}","start":0,"end":290,"selection":{"wholeText":"${text}","start":0,"end":290}}`,
+    method: 'POST'
+  })
+  const data = (await x.json()) as any
+  console.log(data.suggestions)
+
   const proxies = [
     { url: 'socks5://45.89.19.21:16739@FSOfa5:EZaEVDGtbm' },
     { url: 'socks5://45.89.19.117:17807@FSOfa5:EZaEVDGtbm' },
